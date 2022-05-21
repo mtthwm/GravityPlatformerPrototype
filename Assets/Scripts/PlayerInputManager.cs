ceee51585df1a62f2c7b4593024b5c2e0bcc0bc8
mtthwm <60329320+mtthwm@users.noreply.github.com>
2022-05-20 20:47:19 -0600

    A failed attempt :( but I think the code may be useful

--- Assets/Scripts/PlayerInputManager.cs
+++ Assets/Scripts/PlayerInputManager.cs
@@ -76,24 +76,33 @@ public class PlayerInputManager : MonoBehaviour
 
             Vector2 dir = safeContext.ReadValue<Vector2>();
 
-            Quaternion rotationFromCameraUpToGravityUp = Quaternion.FromToRotation(Camera.main.transform.up, (Vector3) gravityDir);
-
-            Vector3 fixedCameraForward = rotationFromCameraUpToGravityUp * Camera.main.transform.forward;
-
-            Debug.DrawRay(this.transform.position, fixedCameraForward * 3f, Color.cyan);
-
             //Vector3 camForward = Camera.main.transform.forward;
             //float x = Vector3.Dot(camForward, transform.right);
             //float y = Vector3.Dot(camForward, transform.forward);
 
             //Vector2 cameraVec = new Vector2(x, y).normalized;
-            
-
-            testText.text = "" + -Camera.main.transform.rotation.eulerAngles.y;
 
             if (gravityDir != null)
             {
+                Quaternion rotationFromCameraUpToGravityUp = Quaternion.FromToRotation(Camera.main.transform.up, (Vector3)gravityDir);
+                Vector3 adjustedCameraForward = rotationFromCameraUpToGravityUp * Camera.main.transform.forward;
+
+                Debug.DrawRay(this.transform.position, adjustedCameraForward * 3f, Color.cyan);
+                Debug.DrawRay(this.transform.position, (Vector3)gravityDir * 3f, Color.cyan);
+
                 Vector3 realMoveDir = Utils.Align2DVector((Vector3)gravityDir, dir);
+
+                Vector3 adjustedWorldForward = Quaternion.FromToRotation(Vector3.up, (Vector3)gravityDir) * Vector3.forward;
+
+                Debug.DrawRay(this.transform.position, adjustedWorldForward * 3f, Color.yellow);
+                Debug.DrawRay(this.transform.position, (Vector3)gravityDir * 3f, Color.yellow);
+
+                Quaternion necessaryRotation = Quaternion.FromToRotation(adjustedCameraForward, adjustedWorldForward);
+
+                Debug.DrawRay(this.transform.position, realMoveDir * 3f, Color.red);
+                realMoveDir = necessaryRotation * realMoveDir;
+                Debug.DrawRay(this.transform.position, realMoveDir * 3f, Color.black);
+
                 gravityAffectedMovement.Move(realMoveDir);
             }
         }
