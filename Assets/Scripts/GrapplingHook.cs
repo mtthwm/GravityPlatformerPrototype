e58b43d1ba6efed7564b25fe07310addb7d38263
mtthwm <60329320+mtthwm@users.noreply.github.com>
2022-06-03 21:57:18 -0600

    Working on hookshot
    Tried locking the camera itself when using the hookshot, but couldn't figure it out. Then I tried disabling mouse input, but that didn't work for some reason....

--- Assets/Scripts/GrapplingHook.cs
+++ Assets/Scripts/GrapplingHook.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class GrapplingHook : Gun
 {
+    public delegate void GrappleStartAction();
+    public static event GrappleStartAction OnGrappleStart;
+    public delegate void GrappleEndAction();
+    public static event GrappleEndAction OnGrappleEnd;
+
     [SerializeField] GravityAffectedMovement gravityMovement;
     [SerializeField] float pullAccelleration = 5f;
 
     protected override void ResolveHit (RaycastHit hit)
     {
+        OnGrappleStart?.Invoke();
         gravityMovement.Accellerate((hit.point - gravityMovement.transform.position).normalized * pullAccelleration);
     }
 
     public override void EndShoot ()
     {
+        OnGrappleEnd?.Invoke();
         base.EndShoot();
         gravityMovement.EndAccelleration();
     }
