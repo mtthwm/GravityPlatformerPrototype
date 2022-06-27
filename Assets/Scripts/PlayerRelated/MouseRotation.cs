29e1a95118bfd4cfb4bb2e421ae45d1087a76612
mtthwm <60329320+mtthwm@users.noreply.github.com>
2022-06-26 22:42:13 -0600

    Added a limit to player look controls

--- Assets/Scripts/PlayerRelated/MouseRotation.cs
+++ Assets/Scripts/PlayerRelated/MouseRotation.cs
@@ -9,7 +9,7 @@ public class MouseRotation : MonoBehaviour
     [SerializeField] Transform body;
     [Range(0, 1f)] public float XSensitivity;
     [Range(0, 1f)] public float YSensitivity;
-    public float LimitYRotation = 90;
+    public float LimitYRotation = 45;
     public bool InvertY;
     public bool InvertX;
 
@@ -22,8 +22,23 @@ public class MouseRotation : MonoBehaviour
 
     public void Rotate (Vector2 direction)
     {
-        
-        playerCamera.Rotate(new Vector3(direction.y * (InvertY ? 1 : -1) * YSensitivity, 0, 0));
+        Vector3 baselineForward = Vector3.Cross(playerCamera.right, gravityAffected.GetGravityDirection());
+        Vector3 upperLimitVector = Quaternion.AngleAxis(LimitYRotation, playerCamera.right) * baselineForward;
+        Vector3 lowerLimitVector = Quaternion.AngleAxis(-LimitYRotation, playerCamera.right) * baselineForward;
+
+        Debug.DrawRay(transform.position, upperLimitVector, Color.red);
+        Debug.DrawRay(transform.position, lowerLimitVector, Color.red);
+
+        float upperMaxRotation;
+        Quaternion.FromToRotation(playerCamera.forward, upperLimitVector).ToAngleAxis(out upperMaxRotation, out _);
+        float lowerMaxRotation;
+        Quaternion.FromToRotation(playerCamera.forward, lowerLimitVector).ToAngleAxis(out lowerMaxRotation, out _);
+
+        Debug.Log(lowerMaxRotation + " " + upperMaxRotation);
+
+        float realRotation = Mathf.Clamp(direction.y * (InvertY ? 1 : -1) * YSensitivity, -lowerMaxRotation, upperMaxRotation);
+
+        playerCamera.Rotate(new Vector3(realRotation, 0, 0));
         body.Rotate(new Vector3(0, direction.x * (InvertX ? -1 : 1) * XSensitivity, 0));
     }
 }
